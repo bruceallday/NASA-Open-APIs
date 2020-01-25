064e71b17752ca0d12277a5008f9723d1dd12155
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-01-25 14:48:57 -0500

    feat: GridListTileBar to RoverImage Component with Earth date

--- src/components/app/app.component.js
+++ src/components/app/app.component.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import Loader from '../loader/loader.component.js'
-import Grid from '@material-ui/core/Grid'
 
 import GridList from "@material-ui/core/GridList";
 import GridListTile from '@material-ui/core/GridListTile';
@@ -9,15 +8,14 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 import RoverImages from '../roverimage/roverimage.component.js'
 import SearchForm from '../searchform/searchform.component.js'
 
-
 import './app.styles.css'
+
 
 
 const App = () => {
 
     const [data, setData] = useState(null)
     const [isLoading, setLoading] = useState(false)
-
 
     const getData = (sol, camera, rover) => {
         console.log("UPDATED_APP")
@@ -31,22 +29,23 @@ const App = () => {
             return result.json();
           })
           .then(data => {
-            console.log("noerror");
             console.log(data);
             // setLoading(false)
 
             if (data.error) {
-              console.log(data.error);
-              setLoading(false);
+                console.log(data.error);
+                setLoading(false);
+
             } else if (data.photos < 1) {
-              console.log("EMPTY_ARRAY");
-              alert("No images for this sol");
-              setLoading(false);
+                console.log("EMPTY_ARRAY");
+                alert("No images for this sol");
+                setLoading(false);
               return;
+
             } else {
-              setData(data);
-              console.log(data);
-              setLoading(false);
+                setData(data);
+                console.log(data);
+                setLoading(false);
             }
           });
     }
@@ -65,7 +64,11 @@ const App = () => {
           {data ? (
             data.photos.map((img, i) => (
               <GridListTile key={i} item>
-                <RoverImages key={i} imageUrl={img.img_src} />
+                <RoverImages
+                  key={i}
+                  imageUrl={img.img_src}
+                  earthDate={img.earth_date}
+                />
               </GridListTile>
             ))
           ) : (
--- src/components/roverimage/roverimage.component.js
+++ src/components/roverimage/roverimage.component.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import GridListTileBar from "@material-ui/core/GridListTileBar";
 import './roverimage.styles.css'
 
 const RoverImages = (props) =>{
-    return(
-       <img className="roverImg" src={props.imageUrl} alt='Mars landscape' />
-    )
+    console.log("PROPS:" + props)
+    return (
+      <div>
+        <img className="roverImg" src={props.imageUrl} alt="Mars landscape" />
+        <GridListTileBar title={`Earth Date: ${props.earthDate}`} />
+      </div>
+    );
 }
 
 export default RoverImages
