511ba820265dbae68fdc380cb8eccba01f778e99
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-02-03 22:15:39 -0500

    feat: fetch earth image data and aligned page headings

--- src/components/earth/earth.component.js
+++ src/components/earth/earth.component.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import API_KEY from '../../api-key'
 
 import PageTitle from '../page-title/page-title.component.js'
 
@@ -7,6 +8,32 @@ import { useStyles } from './earth.styles'
 const Earth = () => {
     
     const classes = useStyles()
+    const [data, setData] = useState()
+    const [isLoading, setLoading] = useState()
+
+    useEffect(() => {
+      getData()
+    }, [])
+
+    const getData = async () => {
+      setData(null);
+      setLoading(true);
+
+      const result = await fetch(
+       ` https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=${API_KEY.apiKey}`
+      )
+
+      const data = await result.json();
+
+      if (data.error) {
+        console.log(data);
+        setLoading(false);
+      } else {
+        setData(data);
+        setLoading(false);
+        console.log(data);
+      }
+    }
 
     return (
       <div>
